86cbaf680949b13ab184fd38e2e840cb0721d905
Joshua Sanchez <joshua.sanchez@talosdigital.com>
2020-01-25 22:26:57 -0500

    backend_spring - API advance

---
+++ back/data_register_rest_api/src/main/java/com/data/register/api/data_register_rest_api/models/Register.java
@@ -0,0 +1,4 @@
+package com.data.register.api.data_register_rest_api.models;
+
+public class Register {
+}
---
+++ back/data_register_rest_api/src/main/java/com/data/register/api/data_register_rest_api/models/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.data.register.api.data_register_rest_api.models;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, String> {
+
+}
