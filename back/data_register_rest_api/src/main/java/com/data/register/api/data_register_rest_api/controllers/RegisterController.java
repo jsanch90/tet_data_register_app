0c39b7084d601b915257d66737dc025f11dfcf74
Joshua Sanchez <joshua.sanchez@talosdigital.com>
2020-02-01 23:31:27 -0500

    Secret key config

--- back/data_register_rest_api/src/main/java/com/data/register/api/data_register_rest_api/controllers/RegisterController.java
+++ back/data_register_rest_api/src/main/java/com/data/register/api/data_register_rest_api/controllers/RegisterController.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/register")
+@RequestMapping(path = "register")
 public class RegisterController {
 
     @Autowired
